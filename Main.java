c82ec82ff33ad05035859db92263ad54565e6735
fe1lor <roks.budy@gmail.com>
2022-12-22 23:18:25 +0500

    Create Main.java
    test

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+ int five = 5;
+ boolean name= true;
+ boolean name2=false;
+ float number=5.4F;
+char symbol='Q';
+        System.out.println(five);
+        System.out.println(name);
+        System.out.println(name2);
+        System.out.println(number);
+        System.out.println(symbol);
+    }
+}
